8e9ea90d8ba33ef83cfc37465617682b19c663bf
WaldenRuemmele <ruemmele@mines.edu>
2022-04-18 16:25:28 -0600

    added files for each front end path

--- src/stores.js
+++ src/stores.js
@@ -5,22 +5,21 @@ import { useParams } from "react-router-dom";
 
 function Store() {
   const { storeID } = useParams();
-  const [singleStore, setStore] = useState({});
+  const [stores, setStore] = useState([]);
 
   // render all stores
   useEffect(() => {
     fetch(`http://localhost:8000/stores`)
       .then((body) => body.json())
-      .then((json) => setStore(() => json)
-      );
+      .then((json) => setStore(() => [...json]));
   }, [storeID]);
   
-  console.log(singleStore)
+  console.log(stores)
 
   return (
     <>
       <h1>
-        <p>{singleStore.Name}</p>
+         <div>{stores.map((store) => (<p>{store.name}</p>))}</div>
       </h1>
     </>
   );
