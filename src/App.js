71a25a516320a81a3e947c6435dcaa3bbf2304be
CodexVeritas <wilson.benjamin1000@gmail.com>
2022-04-18 16:58:59 -0600

    Adding new naming stuff

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Store from "./stores.js";
 
-import StoreID from "./stores.js";
-import StoresIDNew from "./stores.js";
-import StoresIDItemID from "./stores.js";
+import { StoreIDItem } from "./stores.js";
+import { StoresIDNew } from "./stores.js";
+import { StoresIDItemID } from "./stores.js";
 
 function App() {
 
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/stores" element={<Store/>}/>
 
-          <Route path="/stores/:store_id" element={<StoreID/>}/>
+          <Route path="/stores/:store_id" element={<StoreIDItem/>}/>
           <Route path="/stores/:store_id/items/new" element={<StoresIDNew/>}/>
           <Route path="/stores/:store_id/items/:item_id" element={<StoresIDItemID/>}/>
 
